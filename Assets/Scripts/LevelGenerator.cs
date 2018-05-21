e5e444bda4b6386baab67a86b000f085c1fed71f
sebatian003peru <sebastian003peru@gmail.com>
2018-05-21 12:35:58 -0500

    Started new Repository with group

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+    //public enum TileType {
+    //typeA, typeB, typeC
+    //}
+
+    //public int minTile =0;
+    //public int maxTile =1;
+    //public GameObject[] tiles;
+    //public TileType[] pattern;
+
+    bool selfDestruct = false;
+    public GameObject[] roomAvalaible;
+    public GameObject testing;
+    int roomIndex;
+    public Transform roomSpawn;
+    public int totalRoomsSpawned = 0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            GenerateStage();
+        }
+
+        if (totalRoomsSpawned >= 2)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+        void OnTriggerEnter(Collider col)
+        {
+            if (col.gameObject.tag == "Player")
+            {
+                GenerateStage();
+
+            }
+
+        }
+    
+
+
+	public void GenerateStage()
+	{
+        
+        roomIndex = Random.Range(0,4);
+		//Vector3 starPosition = roomSpawn.transform.position;
+		//Vector3 position = starPosition;
+		//Quaternion rotation = transform.rotation;
+		GameObject clone = Instantiate(roomAvalaible[roomIndex], roomSpawn.position, roomSpawn.rotation);
+        //position = clone.transform.Find("exit").position;
+        //rotation = clone.transform.Find("exit").rotation;
+        totalRoomsSpawned += 1;
+        //foreach(int p in pattern){	
+
+        //GameObject tile = tiles[p];
+        //GameObject clone = Instantiate(tile, position, rotation);
+
+
+        //selfDestruct = true;
+
+
+    }
+
+
+	}
+
+
+
+
+
+
---
+++ Assets/Scripts/SaveScore.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveScore : MonoBehaviour {
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
