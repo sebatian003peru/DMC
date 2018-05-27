92e3103f710920ae0ce19987207f238d9155552e
sebatian003peru <sebastian003peru@gmail.com>
2018-05-26 21:58:03 -0500

    Mejorar menu Pause

--- Assets/Scripts/Menu/canvas audio/menuAudio.cs
+++ Assets/Scripts/Menu/canvas audio/menuAudio.cs
@@ -5,57 +5,27 @@ using UnityEngine.SceneManagement;
 public class menuAudio : MonoBehaviour
 {
 
-    public GameObject PauseUI;
-    public GameObject VolSlider;
-    public bool paused = false;
-    public bool pressed = false;
 
+			
+	public void Pause()
 
-    void Start()
-    {
+	{
+		Debug.Log(Time.timeScale);
+		Time.timeScale = 0;
+	}
 
-
-
-        PauseUI.SetActive(false);
-
-    }
-
-    void Update()
-    {
-
-        if (Input.GetButtonDown("Pause"))
-        {
-            paused = !paused;
-        }
-        if (paused)
-        {
-            Debug.Log(Time.timeScale);
-            PauseUI.SetActive(true);
-            Time.timeScale = 0;
-        }
-        /*if (!paused)
-        {
-            PauseUI.SetActive(false);
-            Time.timeScale = 1;
-        }*/
-
-    }
 
     public void Resume()
     {
-        paused = false;
-        PauseUI.SetActive(false);
+        
+       
         Debug.Log(Time.timeScale);
         Time.timeScale = 1;
         Debug.Log(Time.timeScale);
-        VolSlider.SetActive(false);
+       
     }
 
-    public void VolShow()
-    {
-        VolSlider.SetActive(true);
 
-    }
 
 
 
