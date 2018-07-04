dd0e74eb9c3b95337a0bdf9c474059b2de895998
Alfsaram <varko2-34@hotmail.com>
2018-07-04 02:42:04 -0500

    se arreglo finalmente el bug de la musica

--- Assets/Scripts/Modelo/HeroesNeverDie.cs
+++ Assets/Scripts/Modelo/HeroesNeverDie.cs
@@ -2,9 +2,20 @@
 using UnityEngine;
 
 public class HeroesNeverDie : MonoBehaviour {
-     
+
+    public static HeroesNeverDie Instance;
+
 	// Use this for initialization
 	void Awake () {
-        DontDestroyOnLoad(this.gameObject);
+
+        if (Instance)
+        {
+            DestroyImmediate(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
 	}
 }
